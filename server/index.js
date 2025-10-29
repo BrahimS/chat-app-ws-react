8b4bec7cfc63be8947eca15104238c13977018e3
Sourny <bsourny@gmail.com>
2025-10-29 16:11:44 +0100

    fix(server): update the broadcast function

--- server/index.js
+++ server/index.js
@@ -11,14 +11,12 @@ wss.on('connection', (ws) => {
     console.log(`message reçu: ${message.toString()}`);
 
     // Diffuser le message à tous les clients
-    ws.clients.forEach((client) => {
-      console.log(client.readyState)
-      if(client.readyState === ws.OPEN) {
-      client.send(message.toString());
-    }
+    wss.clients.forEach((client) => {
+      if (client.readyState === ws.OPEN) {
+        client.send(message.toString());
+      }
+    });
   });
-  }
-  );
   ws.on('close', () => {
     console.log('Client déconnecté');
   })
